e706529dc6cf61de08822f96dfd60fb54393b808
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-18 13:35:13 +0300

    Added confirm email function

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+import datetime
 from app import app
 from flask import render_template, flash, redirect, request, url_for
 from .token import confirm_token, generate_confirmation_token
@@ -42,3 +43,23 @@ def register():
             return redirect(url_for("email_verification_sent"))
 
     return render_template('Register.html', form=form)
+
+@app.route('/confirm/<token>')
+def confirm_email(token):
+    if User.confirmed==1:
+        flash('✅ Account Already Confirmed! You Can Log In.', 'success')
+        return redirect(url_for('login'))
+
+    email = confirm_token(token)
+    user = User.query.filter_by(email=email).first_or_404()
+
+    if user.email == email:
+        user.confirmed = True
+        user.confirmed_on = datetime.datetime.now()
+        db.session.add(user)
+        db.session.commit()
+        flash('✅ You Have Successfully Confirmed Your Email Address. You Can Now Log In. Thanks!', 'success')
+    else:
+        flash('⚠️ The Confirmation Link Is Invalid Or Has Expired.', 'danger')
+
+    return redirect(url_for('login'))
