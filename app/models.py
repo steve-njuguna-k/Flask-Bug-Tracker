619ce722eb1dac6c3e6a05e384d6237d1d6b72be
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-18 13:22:23 +0300

    Added register function & models

---
+++ app/models.py
@@ -0,0 +1,125 @@
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+post_tags = db.Table('post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('posts.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'))
+)
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(256), unique=True)
+    bugs = db.relationship('Bugs',backref = 'user',lazy = "dynamic")
+    comments = db.relationship('Comments',backref = 'user',lazy = "dynamic")
+    profile_pic = db.Column(db.String(500), nullable=False, default='https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png')
+    registered_on = db.Column(db.DateTime, nullable=False)
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed_on = db.Column(db.DateTime, nullable=True)
+
+    def __init__(self, username, email, password, confirmed, confirmed_on=None):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.registered_on = datetime.datetime.now()
+        self.confirmed = confirmed
+        self.confirmed_on = confirmed_on
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def __repr__(self):
+        return f'User: {self.username}'
+
+class Bugs(db.Model):
+    __tablename__ = 'bugs'
+
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(20000), nullable=False)
+    comment = db.relationship('Comments', backref='post', lazy='dynamic')
+    tags = db.relationship('Tags',secondary=post_tags, back_populates="posts")
+    author = db.Column(db.Integer,db.ForeignKey('users.id'))
+    is_resolved = db.Column(db.Boolean, nullable=False, default=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    created_on = db.Column(db.DateTime, default = datetime.datetime.utcnow())
+    updated_on = db.Column(db.DateTime, nullable=True)
+
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+        self.registered_on = datetime.datetime.now()
+
+    def __repr__(self):
+        return '<Bugs: {}>'.format(self.description)
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key=True)
+    comment = db.Column(db.String(1000), nullable=False)
+    bug = db.Column(db.Integer, db.ForeignKey('bugs.id'))
+    date_published = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+    user = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+    def __init__(self, comment):
+        self.comment = comment
+
+    def __repr__(self):
+        return '<Comment: {}>'.format(self.comment)
+
+class Tags(db.Model):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    bugs = db.relationship('Bugs', secondary = post_tags, back_populates = "tags")
+    created_on = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.id, self.name)
+
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    bug_id = db.Column(db.Integer,db.ForeignKey('bugs.id'))
+
+    @classmethod
+    def get_upvotes(cls,id):
+        upvote = Upvote.query.filter_by(bug_id=id).all()
+        return upvote
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.bug_id}'
+
+class Downvote(db.Model):
+    __tablename__ = 'downvotes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    bug_id = db.Column(db.Integer,db.ForeignKey('bugs.id'))
+    
+    @classmethod
+    def get_downvotes(cls,id):
+        downvote = Downvote.query.filter_by(bug_id=id).all()
+        return downvote
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.bug_id}'
--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, request, url_for
+from .token import confirm_token, generate_confirmation_token
+from flask_bcrypt import Bcrypt
+from .email import send_email
 from .forms import LoginForm, RegisterForm
+from .models import db, User
+
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def home():
@@ -11,7 +17,28 @@ def login():
     form = LoginForm()
     return render_template('Log In.html', form = form)
 
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
-    form = RegisterForm()
-    return render_template('Register.html', form = form)
+    form = RegisterForm(request.form)
+    if form.validate_on_submit():
+        password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=password, confirmed=False)
+        
+        email = User.query.filter_by(email=form.email.data).first()
+        if email:
+            flash ("⚠️ The Email Address Already Exists! Choose Another One", "danger")
+            return redirect(url_for("register"))
+        
+        else:
+            db.session.add(user)
+            db.session.commit()
+
+            token = generate_confirmation_token(user.email)
+            confirm_url = url_for('confirm_email', token=token, _external=True)
+            html = render_template('Activation.html', confirm_url=confirm_url)
+            subject = "[PITCH DECK] Confrim Your Email Address"
+            send_email(user.email, subject, html)
+
+            return redirect(url_for("email_verification_sent"))
+
+    return render_template('Register.html', form=form)
