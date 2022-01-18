7f4e6033f4a19908a6029e0a2001dc715b414094
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-18 13:24:48 +0300

    Made changes to register form

--- app/forms.py
+++ app/forms.py
@@ -9,8 +9,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField(label=('Log In'))
 
 class RegisterForm(FlaskForm):
-    first_name = StringField(label='First Name', validators=[DataRequired(), Length(min=3, max=255,  message='⚠️ Name length must be between %(min)d and %(max)d characters!')], render_kw={"placeholder": "First Name"})
-    last_name = StringField(label='Last Name', validators=[DataRequired(), Length(min=3, max=255,  message='⚠️ Name length must be between %(min)d and %(max)d characters!')], render_kw={"placeholder": "Last Name"})
+    username = StringField(label='Username', validators=[DataRequired(), Length(min=3, max=255,  message='⚠️ Username length must be between %(min)d and %(max)d characters!')], render_kw={"placeholder": "Username"})
     email = StringField(label='Email Address', validators=[DataRequired(), Email(message='⚠️ Enter A Valid Email Address!')], render_kw={"placeholder": "Email Address"})
     password = PasswordField(label='Password', validators=[DataRequired(), Length(min=6, max=255,  message='⚠️ Password strength must be between %(min)d and %(max)d characters!')], render_kw={"placeholder": "Password"})
     confirm_password = PasswordField(label='Confirm Password', validators=[DataRequired(), EqualTo('password', message='⚠️ The Passwords Entered Do Not Match!')], render_kw={"placeholder": "Confirm Password"})
